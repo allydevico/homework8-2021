35adddd1e61062fb6971838d5fdd8c560b376f6f
allydevico <71455118+allydevico@users.noreply.github.com>
2021-12-02 23:36:32 -0500

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -3,7 +3,7 @@ var radius = 15;
 var x = 50;
 var y =150;
 
-var canvas = document.querySelector("#canvas")
+var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
 var colorPicker = document.querySelector("input");
 
@@ -15,29 +15,28 @@ var defaultColor = true;
 
 //Add a listener for loading the window
 window.addEventListener("load", function(){
-	canvas.width = 0.75 * window.innerWidth;
+	canvass.width = 0.75 * window.innerWidth;
 	canvas.height = 0.75 * window.innerHeight;
 	console.log("Window Loaded.");
-})
+});
 
+//Resized the window
 window.addEventListener("resize", function(){
 	ctx.canvas.width  = 0.75*window.innerWidth;
 	ctx.canvas.height = 0.75*window.innerHeight;
-	console.log("Window Resized.")
+	console.log("Window Resized.");
 });
 
 
 //Add a listener for the mouse movement (started below)
 canvas.addEventListener('mousemove', function(e){
-	console.log(this);
-	console.log(e)
 	const x = e.clientX;
 	const y = e.clientY;
 	if (draws){
 		draw(x, y, 5);
 		switchColor();
 	}
-})
+});
 
 //Add a listener for the key events (started below)
 
@@ -45,7 +44,7 @@ canvas.addEventListener('mousemove', function(e){
 //Add a listener for the keydown
 document.addEventListener('keydown', function(e){
 	console.log(this);
-	console.log(e)
+	console.log(e);
 	if (e.key == ' '){
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		console.log("Cleared Canvass");
@@ -58,7 +57,7 @@ document.addEventListener('keydown', function(e){
 		draws = true;
 		console.log("Pen down");
 	}
-})
+});
 
 // Functions!
 
@@ -87,7 +86,7 @@ function switchColor(){
 	colorPicker.addEventListener('change', function(){
 	defaultColor = false;
 	ctx.fillStyle = colorPicker.value;
-	console.log("Changed Color.");})
+	console.log("Changed Color.");});
 }
 
 // I would add a function for draw
